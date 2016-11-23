c2e357c548358bac2a61bbd681750d058abde429
Brian Faust <hello@brianfaust.de>
2016-11-23 12:20:59 +0000

    Applied fixes from StyleCI
    [ci skip] [skip ci]

--- src/Http/Controllers/StatusController.php
+++ src/Http/Controllers/StatusController.php
@@ -1,5 +1,14 @@
 <?php
 
+/*
+ * This file is part of Laravel TurnKey.
+ *
+ * (c) Brian Faust <hello@brianfaust.de>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 namespace BrianFaust\TurnKey\Http\Controllers;
 
 class StatusController extends BaseController
