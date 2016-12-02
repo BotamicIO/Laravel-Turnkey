6aace45dda322c3a305bd6400dea678df08beb9a
Brian Faust <hello@brianfaust.de>
2016-12-02 20:23:15 +0200

    Rename Service Provider

--- src/TurnKeyServiceProvider.php
+++ src/TurnKeyServiceProvider.php
@@ -11,7 +11,9 @@
 
 namespace BrianFaust\TurnKey;
 
-class ServiceProvider extends \BrianFaust\ServiceProvider\ServiceProvider
+use BrianFaust\ServiceProvider\ServiceProvider;
+
+class TurnKeyServiceProvider extends ServiceProvider
 {
     public function boot()
     {
