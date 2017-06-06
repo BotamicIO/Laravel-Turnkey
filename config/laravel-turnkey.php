7a6fb847d50048a927876068eddc8beff9f167c8
Brian Faust <hello@brianfaust.de>
2017-06-06 08:04:44 +0000

    Apply fixes from StyleCI
    [ci skip] [skip ci]

--- config/laravel-turnkey.php
+++ config/laravel-turnkey.php
@@ -16,19 +16,19 @@ return [
     'identifier' => 'email',
 
     'feedback' => [
-        'active' => true,
+        'active'  => true,
         'subject' => 'Feedback by lost customer',
-        'view' => 'turnkey::feedback-mail',
-        'email' => 'feedback@myapp.io',
+        'view'    => 'turnkey::feedback-mail',
+        'email'   => 'feedback@myapp.io',
     ],
 
     'urls' => [
-        'index' => '/account/close',
-        'handle' => '/account/close',
+        'index'   => '/account/close',
+        'handle'  => '/account/close',
         'goodbye' => '/account/close/success',
         'staying' => '/account/close/failure',
 
-        'feedback' => '/account/close/feedback',
+        'feedback'        => '/account/close/feedback',
         'feedback-handle' => '/account/close/feedback',
     ],
 
